240c2f807bceba20130f6eecb8ce6cd5f5bc72e8
Ido Ben-Tsion <ibentsion@gmail.com>
2026-07-02 00:01:05 +0300

    less prints

--- train_gpt2.py
+++ train_gpt2.py
@@ -351,7 +351,7 @@ with open(log_file, "w") as f: # open for writing to clear the file
     pass
 
 loss_accum = val_loss_accum = None
-VAL_PRINT_RATIO=20
+VAL_PRINT_RATIO=50
 max_norm = 3e3
 
 for step in range(max_steps):
@@ -385,7 +385,8 @@ for step in range(max_steps):
     tokens_processed = train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size
     tokens_per_sec = tokens_processed / dt
     if master_process:
-        print(f"step {step:5d} | avg_loss: {loss_accum.item()/(1+step):.6f} | batch_loss: {loss.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
+        if 0 == step % (VAL_PRINT_RATIO//4):
+            print(f"step {step:5d} | avg_loss: {loss_accum.item()/(1+step):.6f} | batch_loss: {loss.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
         with open(log_file, "a") as f:
             f.write(f"{step} train {loss_accum.item():.6f}\n")
 
